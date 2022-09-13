91bf6dcb967e3f2cd86caedf455bf479c29c51e2
James A Birrell Gray <james@jamesbirrellgray.com>
2022-09-13 12:44:42 +1000

    reafctor: paginator moved to utils

---
+++ src/utils/paginator.ts
@@ -0,0 +1,19 @@
+const Paginator = (items, page, per_page) => {
+  var page = page || 1,
+    per_page = per_page || 11,
+    offset = (page - 1) * per_page,
+    paginatedItems = items.slice(offset).slice(0, per_page),
+    total_pages = Math.ceil(items.length / per_page),
+    nextPage = parseInt(page)+1;
+  return {
+    page: page,
+    per_page: per_page,
+    pre_page: parseInt(page) - 1 ? parseInt(page) - 1 : null,
+    next_page: total_pages > page ? nextPage : null,
+    total: items.length,
+    total_pages: total_pages,
+    data: paginatedItems,
+  };
+}
+
+export default Paginator;
